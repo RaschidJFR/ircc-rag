b6cc334dced5d009abd94cd36e76ff8b869e86ed
Raschid JF Rafaelly <me@raschidjfr.dev>
2025-08-26 23:57:05 -0400

    fix(rag): fixed zod validation error in `sanitizedQuery()`

--- src/lib/rag.ts
+++ src/lib/rag.ts
@@ -54,8 +54,8 @@ export async function sanitizeQuery(query: string, messageHistory = [], model = 
   const { output, error } = await model
     .withStructuredOutput(
       z.object({
-        output: z.string().nullable().describe('The reformulated query. Empty if an error is raised'),
-        error: z.string().optional().describe('The error, if any'),
+        output: z.string().describe('The reformulated query. Empty if an error is raised'),
+        error: z.string().describe('The error, if any'),
       })
     )
     .invoke(prompt);
@@ -134,11 +134,9 @@ ${JSON.stringify(history, null, 2)}
 
   const { answer } = await model
     .withStructuredOutput(
-      z
-        .object({
-          answer: z.array(zParagraph).describe('Array of answer paragraphs'),
-        })
-        .required()
+      z.object({
+        answer: z.array(zParagraph).describe('Array of answer paragraphs'),
+      })
     )
     .invoke(prompt);
 
